3a509f7ee492fddc8fbad63a23e1a92a058268ab
AwrenNuit <a.rennaker@yahoo.com>
2020-01-05 13:41:49 -0600

    testing mui theme toggle options

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import TimerForm from '../TimerForm/TimerForm';
 import Timer from '../Timer/Timer';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
+
+  const [theme, setTheme] = useState({
+    palette: {
+      type: `light`
+    }
+  });
+
+  const toggleDarkTheme = () => {
+    console.log('in there');
+    let newPaletteType = theme.palette.type === `light` ? `dark` : `light`;
+    setTheme({
+      palette: {
+        type: newPaletteType
+      }
+    });
+  };
+
+  const muiTheme = createMuiTheme(theme);
+
   return (
       <Router>
-        <Route exact path="/" component={TimerForm} />
-        <Route path="/timer" component={Timer} />
+        <MuiThemeProvider theme={muiTheme}>
+          <Route exact path="/" component={TimerForm} />
+          <Route path="/timer" component={Timer} />
+        </MuiThemeProvider>
       </Router>
   );
 }
